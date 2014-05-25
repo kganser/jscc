c1310ff42afa429dc742785305692db24c488b83
kganser <kganser@kganser.com>
2014-05-24 22:39:18 -0700

    State machine serialization/deserialization

--- src/jscc.js
+++ src/jscc.js
@@ -4,209 +4,255 @@
 
 kernel.add('jscc', function() {
   return function(grammar, start, tokens) {
+  
+    var symbols = {}, states = [];
+  
+    if (Array.isArray(start)) {
     
-    var symbols = {}, nonterminals = Object.keys(grammar),
-        firsts = {}, states = [], oldStart = start, done;
-
-    // utility functions
-    var unique = function(symbol, obj) {
-      while (symbols.hasOwnProperty(symbol)) symbol += "'";
-      if (obj) obj[symbol] = 1;
-      return obj || symbol;
-    };
-    
-    var merge = function(to, from, returnAdded) {
-      var added;
-      Object.keys(from).forEach(function(key) {
-        if (!to.hasOwnProperty(key))
-          added = to[key] = 1;
+      start[0].forEach(function(symbol) { symbols[symbol] = 1; });
+      (states = start.slice(1).map(function(state) {
+        return {transitions: {}, reductions: {}, state: state};
+      })).forEach(function(state, i) {
+        var t = Array.isArray(state.state) ? state.state[0] : state.state,
+            r = Array.isArray(state.state) ? state.state[1] : {};
+        Object.keys(t).forEach(function(symbol) {
+          state.transitions[start[0][symbol-1]] = states[t[symbol]];
+        });
+        Object.keys(r).forEach(function(symbol) {
+          var value = r[symbol],
+              nonterminal = start[0][Array.isArray(value) ? value[0]-1 : value-1],
+              production = nonterminal ? grammar[nonterminal][Array.isArray(value) ? value[1] : 0] : [[Object.keys(states[0].transitions)[0]], function(e) { return e; }];
+          state.reductions[+symbol ? start[0][symbol-1] : ''] = [nonterminal, production[0], null, null, production[1]];
+        });
+        delete state.state;
       });
-      return returnAdded ? added : to;
-    };
     
-    var stringify = function(item) {
-      return item[0]+' → '+item[1].map(function(symbol, i) {
-        return i != item[2] ? i ? ' '+symbol : symbol : '•'+symbol;
-      }).join('')+(item[2] == item[1].length ? '•' : '');
-    };
+    } else {
     
-    var getFirsts = function(production, start) {
-      var symbol, current = {'': 1};
-      for (var i = start || 0; (symbol = production[i]) && current.hasOwnProperty(''); i++) {
-        delete current[''];
-        if (grammar.hasOwnProperty(symbol)) {
-          merge(current, firsts[symbol]);
-        } else {
-          current[symbol] = 1;
-        }
-      }
-      return current;
-    };
-    
-    // validate, prepare grammar
-    nonterminals.forEach(function(nonterminal) {
-      firsts[nonterminal] = {};
-      symbols[nonterminal] = 1;
-      grammar[nonterminal].forEach(function(production) {
-        for (var symbol, i = 0; i < production[0].length; i++) {
-          if (symbol = production[0][i]) {
-            symbols[symbol] = 1;
-          } else { // empty strings are reserved as EOF token
-            production[0].splice(i--, 1);
+      var nonterminals = Object.keys(grammar), firsts = {}, oldStart = start, done;
+
+      // utility functions
+      var unique = function(symbol, obj) {
+        while (symbols.hasOwnProperty(symbol)) symbol += "'";
+        if (obj) obj[symbol] = 1;
+        return obj || symbol;
+      };
+      
+      var merge = function(to, from) {
+        var added;
+        Object.keys(from).forEach(function(key) {
+          if (!to.hasOwnProperty(key))
+            added = to[key] = 1;
+        });
+        return added;
+      };
+      
+      var stringify = function(item) {
+        return item[0]+' → '+item[1].map(function(symbol, i) {
+          return i != item[2] ? i ? ' '+symbol : symbol : '•'+symbol;
+        }).join('')+(item[2] == item[1].length ? '•' : '');
+      };
+      
+      var getFirsts = function(production, start) {
+        var symbol, current = {'': 1};
+        for (var i = start || 0; (symbol = production[i]) && current.hasOwnProperty(''); i++) {
+          delete current[''];
+          if (grammar.hasOwnProperty(symbol)) {
+            merge(current, firsts[symbol]);
+          } else {
+            current[symbol] = 1;
           }
         }
-      });
-    });
-    
-    if (!grammar.hasOwnProperty(oldStart)) throw 'Start symbol does not appear in grammar';
-    grammar[start = unique(oldStart)] = [[[oldStart], function(e) { return e; }]];
-    
-    Object.keys(tokens = tokens || {}).forEach(function(token) {
-      tokens[token] = new RegExp(tokens[token].source.replace(/^\^?/, '^'), tokens[token].ignoreCase ? 'i' : '');
-    });
-    
-    // compute first sets
-    do {
-      done = true;
+        return current;
+      };
+      
+      // validate, prepare grammar
       nonterminals.forEach(function(nonterminal) {
+        firsts[nonterminal] = {};
+        symbols[nonterminal] = 1;
         grammar[nonterminal].forEach(function(production) {
-          if (merge(firsts[nonterminal], getFirsts(production[0]), true))
-            done = false;
+          for (var symbol, i = 0; i < production[0].length; i++) {
+            if (symbol = production[0][i]) {
+              symbols[symbol] = 1;
+            } else { // empty strings are reserved as EOF token
+              production[0].splice(i--, 1);
+            }
+          }
         });
       });
-    } while (!done);
-    
-    //firsts.forEach(function(n) { console.log('first', n, Object.keys(firsts[n])); });
-    
-    // adds any nonkernel productions to complete closure,
-    // as well as all shifting transition symbols
-    var close = function(state) {
-      if (!state.transitions) state.transitions = {};
-      state.reductions = {};
+      
+      if (!grammar.hasOwnProperty(oldStart)) throw 'Start symbol does not appear in grammar';
+      grammar[start = unique(oldStart)] = [[[oldStart], function(e) { return e; }]];
+      
+      Object.keys(tokens = tokens || {}).forEach(function(token) {
+        tokens[token] = new RegExp(tokens[token].source.replace(/^\^?/, '^'), tokens[token].ignoreCase ? 'i' : '');
+      });
+      
+      // compute first sets
       do {
         done = true;
-        state.items.forEach(function(item) {
-          var lookaheads = {}, next = item[1][item[2]];
-          if (next && !state.transitions[next]) state.transitions[next] = 0;
-          if (!next || !grammar.hasOwnProperty(next)) return;
-          if (Object.keys(item[3]).length && (lookaheads = getFirsts(item[1], item[2]+1)).hasOwnProperty('')) {
-            delete lookaheads[''];
-            merge(lookaheads, item[3]);
-          }
-          grammar[next].forEach(function(production) {
-            var last;
-            if (state.items.some(function(item) {
-              last = item;
-              return item[1] == production[0] && !item[2];
-            })) {
-              if (merge(last[3], lookaheads, true))
-                done = false;
-            } else {
-              state.items.push([next, production[0], 0, lookaheads, production[1]]);
+        nonterminals.forEach(function(nonterminal) {
+          grammar[nonterminal].forEach(function(production) {
+            if (merge(firsts[nonterminal], getFirsts(production[0])))
               done = false;
-            }
           });
         });
       } while (!done);
-      return state;
-    };
-    
-    // generate LR(0) states
-    states.push(close({items: [[start, grammar[start][0][0], 0, {}, grammar[start][0][1]]]}));
-    
-    do {
-      done = true;
-      states.forEach(function(state) {
-        Object.keys(state.transitions).forEach(function(symbol) {
-          // find all productions in state with `symbol` as
-          // their next symbol and advance index; these
-          // become kernel of another state, which we add
-          // to `states` if it doesn't already exist
-          var candidate = {items: []};
+      
+      //firsts.forEach(function(n) { console.log('first', n, Object.keys(firsts[n])); });
+      
+      // adds any nonkernel productions to complete closure,
+      // as well as all shifting transition symbols
+      var close = function(state) {
+        if (!state.transitions) state.transitions = {};
+        state.reductions = {};
+        do {
+          done = true;
           state.items.forEach(function(item) {
-            var production = item[1],
-                index = item[2],
-                next = production[index];
-            if (next && next == symbol)
-              candidate.items.push([item[0], production, index+1, {}, item[4]]);
-          });
-          
-          //console.log('state candidate\n'+candidate.items.map(stringify).join('\n'));
-          
-          if (!states.some(function(state) {
-            var compared = 0;
-            return !state.items.some(function(item) {
-              if (!item[2] && item[0] != start) return;
-              compared++;
-              return !candidate.items.some(function(i) {
-                return item[1] == i[1] && item[2] == i[2];
-              });// && !console.log(stringify(item)+' not in new state') || console.log(stringify(item)+' found in new state');
-            }) && compared == candidate.items.length && (candidate = state);
-          })) {
-            states.push(close(candidate));
-            done = false;
-          }
-          state.transitions[symbol] = candidate;
-        });
-      });
-    } while (!done);
-    
-    // generate lookaheads for LR(0) states
-    var lookaheads = [], foo = unique('#', {});
-    states[0].items[0][3] = {'': 1};
-    
-    states.forEach(function(state) {
-      state.items.forEach(function(item) {
-        if (!item[2] && item[0] != start) return;
-        close({items: [item.slice(0, 3).concat([foo])]}).items.forEach(function(i) {
-          var next = i[1][i[2]];
-          if (next) {
-            state.transitions[next].items.some(function(j) {
-              return i[1] == j[1] && i[2]+1 == j[2] && (next = j);
-            });
-            Object.keys(i[3]).forEach(function(symbol) {
-              if (!foo.hasOwnProperty(symbol)) {
-                next[3][symbol] = 1;
-              } else if (item != next) {
-                lookaheads.push([item[3], next[3]]);
+            var lookaheads = {}, next = item[1][item[2]];
+            if (next && !state.transitions[next]) state.transitions[next] = 0;
+            if (!next || !grammar.hasOwnProperty(next)) return;
+            if (Object.keys(item[3]).length && (lookaheads = getFirsts(item[1], item[2]+1)).hasOwnProperty('')) {
+              delete lookaheads[''];
+              merge(lookaheads, item[3]);
+            }
+            grammar[next].forEach(function(production) {
+              var last;
+              if (state.items.some(function(item) {
+                last = item;
+                return item[1] == production[0] && !item[2];
+              })) {
+                if (merge(last[3], lookaheads))
+                  done = false;
+              } else {
+                state.items.push([next, production[0], 0, lookaheads, production[1]]);
+                done = false;
               }
             });
+          });
+        } while (!done);
+        return state;
+      };
+      
+      // generate LR(0) states
+      states.push(close({items: [[start, grammar[start][0][0], 0, {}, grammar[start][0][1]]]}));
+      
+      do {
+        done = true;
+        states.forEach(function(state) {
+          Object.keys(state.transitions).forEach(function(symbol) {
+            // find all productions in state with `symbol` as
+            // their next symbol and advance index; these
+            // become kernel of another state, which we add
+            // to `states` if it doesn't already exist
+            var candidate = {items: []};
+            state.items.forEach(function(item) {
+              var next = item[1][item[2]];
+              if (next == symbol)
+                candidate.items.push([item[0], item[1], item[2]+1, {}, item[4]]);
+            });
+            
+            //console.log('state candidate\n'+candidate.items.map(stringify).join('\n'));
+            
+            if (!states.some(function(state) {
+              var compared = 0;
+              return !state.items.some(function(item) {
+                if (!item[2] && item[0] != start) return;
+                compared++;
+                return !candidate.items.some(function(i) {
+                  return item[1] == i[1] && item[2] == i[2];
+                });// && !console.log(stringify(item)+' not in new state') || console.log(stringify(item)+' found in new state');
+              }) && compared == candidate.items.length && (candidate = state);
+            })) {
+              states.push(close(candidate));
+              done = false;
+            }
+            state.transitions[symbol] = candidate;
+          });
+        });
+      } while (!done);
+      
+      // generate lookaheads for LR(0) states
+      var lookaheads = [], foo = unique('#', {});
+      states[0].items[0][3] = {'': 1};
+      
+      states.forEach(function(state) {
+        state.items.forEach(function(item) {
+          if (!item[2] && item[0] != start) return;
+          close({items: [item.slice(0, 3).concat([foo])]}).items.forEach(function(i) {
+            var next = i[1][i[2]];
+            if (next) {
+              state.transitions[next].items.some(function(j) {
+                return i[1] == j[1] && i[2]+1 == j[2] && (next = j);
+              });
+              Object.keys(i[3]).forEach(function(symbol) {
+                if (!foo.hasOwnProperty(symbol)) {
+                  next[3][symbol] = 1;
+                } else if (item != next) {
+                  lookaheads.push([item[3], next[3]]);
+                }
+              });
+            }
+          });
+        });
+      });
+      
+      do {
+        done = true;
+        lookaheads.forEach(function(pair) {
+          if (merge(pair[1], pair[0]))
+            done = false;
+        });
+      } while (!done);
+      
+      states.forEach(close);
+      
+      /*console.log(states.length+' states:\n\n'+states.map(function(state) {
+        return state.items.map(stringify).join('\n');
+      }).join('\n\n'));*/
+      
+      // detect shift-reduce, reduce-reduce conflicts
+      states.forEach(function(state) {
+        state.items.forEach(function(item) {
+          var next = item[1][item[2]];
+          if (!next) {
+            Object.keys(item[3]).forEach(function(next) {
+              if (state.transitions.hasOwnProperty(next)) {
+                console.log(Object.keys(state.transitions[next].items[0][3]), Object.keys(item[3]));
+                throw 'Shift-reduce conflict on input "'+next+'"\n  '+stringify(state.transitions[next].items[0])+' (shift)\n  '+stringify(item)+' (reduce)'; }
+              if (state.reductions.hasOwnProperty(next)) throw 'Reduce-reduce conflict on input "'+next+'"\n  '+stringify(state.reductions[next])+'\n  '+stringify(item);
+              state.reductions[next] = item;
+            });
+          } else if (state.reductions.hasOwnProperty(next)) {
+            throw 'Shift-reduce conflict on input "'+next+'"\n  '+stringify(item)+' (shift)\n  '+stringify(state.reductions[next])+' (reduce)';
           }
         });
       });
-    });
-    
-    do {
-      done = true;
-      lookaheads.forEach(function(pair) {
-        if (merge(pair[1], pair[0], true))
-          done = false;
-      });
-    } while (!done);
-    
-    states.forEach(close);
-    
-    /*console.log(states.length+' states:\n\n'+states.map(function(state) {
-      return state.items.map(stringify).join('\n');
-    }).join('\n\n'));*/
-    
-    // detect shift-reduce, reduce-reduce conflicts
-    states.forEach(function(state) {
-      state.items.forEach(function(item) {
-        var next = item[1][item[2]];
-        if (!next) {
-          Object.keys(item[3]).forEach(function(next) {
-            if (state.transitions.hasOwnProperty(next)) throw 'Shift-reduce conflict on input "'+next+'"\n  '+stringify(state.transitions[next].items[0])+' (shift)\n  '+stringify(item)+' (reduce)';
-            if (state.reductions.hasOwnProperty(next)) throw 'Reduce-reduce conflict on input "'+next+'"\n  '+stringify(state.reductions[next])+'\n  '+stringify(item);
-            state.reductions[next] = item;
-          });
-        } else if (state.reductions.hasOwnProperty(next)) {
-          throw 'Shift-reduce conflict on input "'+next+'"\n  '+stringify(item)+' (shift)\n  '+stringify(state.reductions[next])+' (reduce)';
-        }
-      });
-    });
+    }
     
     return function(string) {
+    
+      if (string == undefined) {
+        var s = Object.keys(symbols);
+        return [s].concat(states.map(function(state) {
+          var transitions = {},
+              reductions = {};
+          Object.keys(state.transitions).forEach(function(symbol) {
+            transitions[s.indexOf(symbol)+1] = states.indexOf(state.transitions[symbol]);
+          });
+          Object.keys(state.reductions).forEach(function(symbol) {
+            var item = state.reductions[symbol],
+                nonterminal = s.indexOf(item[0])+1,
+                production;
+            if (nonterminal) grammar[s[nonterminal-1]].some(function(rule, i) {
+              return rule[0] == item[1] && (production = i);
+            });
+            reductions[s.indexOf(symbol)+1] = production ? [nonterminal, production] : nonterminal;
+          });
+          return Object.keys(reductions).length ? [transitions, reductions] : transitions;
+        }));
+      }
+    
       var token, match, ignore = tokens[''], substring = string, values = [], stack = [], state = states[0], i = 0;
       
       while (state) {
@@ -244,7 +290,10 @@ kernel.add('jscc', function() {
             index: i,
             line: string.substring(lastNewline, (string+'\n').indexOf('\n', lastNewline)),
             row: newlines ? newlines.length+1 : 1,
-            column: i - lastNewline
+            column: i - lastNewline,
+            toString: function() {
+              return [this.message, this.line, new Array(this.column+1).join(' ')+'^'].join('\n');
+            }
           };
         }
         
